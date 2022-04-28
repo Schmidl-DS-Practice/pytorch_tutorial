c798be3033407b06cc89606ebe1869e89249b6eb
scottschmidl <sjschmidl@gmail.com>
2022-04-28 04:08:39 -0500

    working through code

---
+++ transfer_learning.py
@@ -0,0 +1,161 @@
+# ImageFolder
+# Scheduler
+# Transfer Learning
+
+from pyparsing import Combine
+import torch
+nn = torch.nn
+optim = torch.optim
+lr_scheduler = optim.lr_scheduler
+import numpy as np
+import torchvision
+datasets, models, transforms = (torchvision.datasets,
+                                torchvision.models,
+                                torchvision.transforms)
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+mean = np.array([.485, .456, .406])
+std = np.array([.229, .224, .225])
+
+data_transforms = {"train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                                transforms.RandomHorizontalFlip(),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize(mean, std)]),
+                   "val": transforms.Compose([transforms.Resize(256),
+                                              transforms.CenterCrop(224),
+                                              transforms.ToTensor(),
+                                              transforms.Normalize(mean, std)])}
+
+# import data
+data_dir = 'data/hymenoptera_data'
+sets = ["train", "val"]
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x]) for x in sets}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                              shuffle=True, num_workers=0) for x in sets}
+dataset_sizes = {x: len(image_datasets[x]) for x in sets}
+class_names = image_datasets[sets[0]].classes
+print(class_names)
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in sets:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+        print()
+
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4}')
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+# #### Finetuning the convnet ####
+# # Load a pretrained model and reset final fully connected layer.
+
+# model = models.resnet18(pretrained=True)
+# num_ftrs = model.fc.in_features
+# # Here the size of each output sample is set to 2.
+# # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+# model.fc = nn.Linear(num_ftrs, 2)
+
+# model = model.to(device)
+
+# criterion = nn.CrossEntropyLoss()
+
+# # Observe that all parameters are being optimized
+# optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+# # StepLR Decays the learning rate of each parameter group by gamma every step_size epochs
+# # Decay LR by a factor of 0.1 every 7 epochs
+# # Learning rate scheduling should be applied after optimizer’s update
+# # e.g., you should write your code this way:
+# # for epoch in range(100):
+# #     train(...)
+# #     validate(...)
+# #     scheduler.step()
+
+# step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+# model = train_model(model, criterion, optimizer, step_lr_scheduler, num_epochs=25)
+
+# #### ConvNet as fixed feature extractor ####
+# # Here, we need to freeze all the network except the final layer.
+# # We need to set requires_grad == False to freeze the parameters so that the gradients are not computed in backward()
+# model_conv = torchvision.models.resnet18(pretrained=True)
+# for param in model_conv.parameters():
+#     param.requires_grad = False
+
+# # Parameters of newly constructed modules have requires_grad=True by default
+# num_ftrs = model_conv.fc.in_features
+# model_conv.fc = nn.Linear(num_ftrs, 2)
+
+# model_conv = model_conv.to(device)
+
+# criterion = nn.CrossEntropyLoss()
+
+# # Observe that only parameters of final layer are being optimized as
+# # opposed to before.
+# optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+# # Decay LR by a factor of 0.1 every 7 epochs
+# exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+# model_conv = train_model(model_conv, criterion, optimizer_conv,
+#                          exp_lr_scheduler, num_epochs=25)
